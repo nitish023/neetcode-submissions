a0ea083506ba2c92ef5c3879d068f92cfa51fd7a
Nitish Chawla <121867781+nitish023@users.noreply.github.com>
2026-05-11 11:23:32 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_hash = Counter(s1)
+        s2_hash = Counter(s2[:len(s1)])
+        left = 0
+
+        if s1_hash == s2_hash:
+            return True
+        
+
+        for right in range(len(s1), len(s2)):
+            s2_hash[s2[right]] += 1
+            s2_hash[s2[left]] -= 1
+            if s2_hash[s2[left]] <= 0:
+                del s2_hash[s2[left]]
+            if s1_hash == s2_hash:
+                return True
+            left += 1
+
+        print(s1_hash)
+        print(s2_hash)
+        return False
