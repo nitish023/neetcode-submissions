f7d11adaa59ea25c2d53861854f4ae191c1d4a28
Nitish Chawla <121867781+nitish023@users.noreply.github.com>
2026-07-23 13:58:55 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        dict_s, dict_t = defaultdict(int), defaultdict(int)
+        
+        for i in range(len(s)):
+            dict_s[s[i]] += 1
+            dict_t[t[i]] += 1
+        
+        return dict_s == dict_t
