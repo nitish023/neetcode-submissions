db93b2829688b0f5b66540d7e65fcad9e12fccce
Nitish Chawla <121867781+nitish023@users.noreply.github.com>
2026-07-23 14:11:56 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        dict = defaultdict(list)
+        ans = []
+        
+        for s in strs:
+            char_map = [0] * 26
+            for i in s:
+                pos = ord(i) - ord('a')
+                char_map[pos] += 1
+
+            key = tuple(char_map)
+            dict[key].append(s)
+        
+        return list(dict.values())
